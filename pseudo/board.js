cccdd45d04a22a0901b8c6bda52ecfb746f96d2f
Snorro1998 <tjaard.van.verseveld@gmail.com>
2021-01-26 16:42:48 +0100

    Updated board

--- pseudo/board.js
+++ pseudo/board.js
@@ -1,21 +1,73 @@
 class Board {
-	constructor(game, nTiles) {
+	constructor(game, boardLength) {
 		this.game = game;
 		this.tiles = [];
-		createBoard(nTiles);
+		createBoard(boardLength);
+	}
+	
+	createPath(nTiles, prevTile, nextTile) {
+		let path = [];
+		for (let i = 0; i <  nTiles; i++) {
+			path.push(new Tile(null));
+		}
+		setPrevious(path);
+		setNext(path);
+		if(typeof prevTile !== "undefined") {
+			if (prevTile != null) {
+				prevTile.nextTiles.push(path[0]);
+				path[0].previousTiles.push(prevTile);
+			}
+		}
+		if(typeof nextTile !== "undefined") {
+			if (nextTile != null) {
+				nextTile.previousTiles.push(path[path.length]);
+				path[path.length].nextTiles.push(nextTile);
+			}
+			
+		}
+		return path;
+	}
+	
+	appendPath(nTiles) {
+		//let path;
+		if (tiles.length == 0) {
+			//path = createPath(nTiles);
+			tiles = createPath(nTiles);
+		}
+		else {
+			//path = createPath(nTiles, path[path.length]);
+			//let origPathLength = path.length;
+			//Add path to tiles
+			tiles.push(...createPath(nTiles, tiles[tiles.length]));
+			//tiles[origPathLength].nextTiles.push(tiles[origPathLength + 1]);
+			//tiles[origPathLength + 1].previousTiles.push(tiles[origPathLength]);
+		}	
+	}
+	
+	prependPath(nTiles) {
+		if (tiles.length == 0) {
+			tiles = createPath(nTiles);
+		}
+		else {
+			tiles.push(...createPath(nTiles, null, tiles[0]));
+		}
 	}
 	
 	createBoard(nTiles) {
+		appendPath(20);
+		
+		
 		//Create tiles
-		for (let i = 0; i <  nTiles; i++) {
+		/*for (let i = 0; i <  nTiles; i++) {
 			tiles.push(new Tile(null));
-		}
+		}*/
+		/*
 		setPrevious();
 		setNext();	
 		
 		for (let i = 0; i < game.players.length; i++) {
 			jumpToStart(game.players[i]);
-		}
+		}*/
 	}
 	
 	updatePlayerPosition(player, tile) {
@@ -58,15 +110,15 @@ class Board {
 		updatePlayerPosition(player, tiles[0]);
 	}
 	
-	setPrevious() {
-		for (let i = 1; i < tiles.length; i++) {
-			tiles[i].previousTiles.push(tiles[i - 1]);
+	setPrevious(path) {
+		for (let i = 1; i < path.length; i++) {
+			path[i].previousTiles.push(path[i - 1]);
 		}
 	}
 	
-	setNext() {
-		for (let i = 0; i < tiles.length - 1; i++) {
-			tiles[i].nextTiles.push(tiles[i + 1]);
+	setNext(path) {
+		for (let i = 0; i < path.length - 1; i++) {
+			path[i].nextTiles.push(path[i + 1]);
 		}
 	}
 }
