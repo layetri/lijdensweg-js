0222c557aa12e872d3b9eefaf5a33d7358107c5d
layetri <daniel@mainstreamct.com>
2021-01-27 14:00:41 +0100

    Annotated pseudo board class.

--- pseudo/board.js
+++ pseudo/board.js
@@ -2,26 +2,32 @@ class Board {
 	constructor(game, boardLength) {
 		this.game = game;
 		this.tiles = [];
-		createBoard(boardLength);
+		this.createBoard(boardLength);
 		
 		this.tileWidth = 10;
 		this.tileHeight = 10;
 		this.totalTiles = 0;
 	}
-	
+
+	// Create a single branch and link it to its parent
 	createPath(nTiles, prevTile, nextTile) {
 		let path = [];
+		// Fill the path array
 		for (let i = 0; i <  nTiles; i++) {
 			path.push(new Tile(null));
 		}
-		setPrevious(path);
-		setNext(path);
+		// Set the direct previous neighbour
+		this.setPrevious(path);
+		// Set the direct next neighbour
+		this.setNext(path);
+		// Find and set any indirect previous neighbours
 		if(typeof prevTile !== "undefined") {
 			if (prevTile != null) {
 				prevTile.nextTiles.push(path[0]);
 				path[0].previousTiles.push(prevTile);
 			}
 		}
+		// Find and set any indirect next neighbours
 		if(typeof nextTile !== "undefined") {
 			if (nextTile != null) {
 				nextTile.previousTiles.push(path[path.length]);
@@ -31,30 +37,33 @@ class Board {
 		}
 		return path;
 	}
-	
+
+	// Append a number of tiles in a straight line
 	appendPath(nTiles) {
 		//let path;
-		if (tiles.length == 0) {
-			tiles = createPath(nTiles);
+		if (this.tiles.length === 0) {
+			this.tiles = this.createPath(nTiles);
 		}
 		else {
 			//Add path to tiles
-			tiles.push(...createPath(nTiles, tiles[tiles.length]));
+			this.tiles += this.createPath(nTiles, this.tiles[this.tiles.length]);
 		}	
 	}
-	
+
+	// Prepend a number of tiles in a straight line
 	prependPath(nTiles) {
-		if (tiles.length == 0) {
-			tiles = createPath(nTiles);
+		if (this.tiles.length === 0) {
+			this.tiles = this.createPath(nTiles);
 		}
 		else {
-			tiles.splice(0, 0, ...createPath(nTiles, null, tiles[0]));
+			this.tiles.splice(0, 0, this.createPath(nTiles, null, this.tiles[0]));
 			//tiles.push(...createPath(nTiles, null, tiles[0]));
 		}
 	}
-	
+
+	// Create a board for n amount of tiles
 	createBoard(nTiles) {
-		appendPath(nTiles);
+		this.appendPath(nTiles);
 	}
 	
 	updatePlayerPosition(player, tile) {
@@ -74,7 +83,7 @@ class Board {
 				return;
 			}
 			else {
-				updatePlayerPosition(player, player.currentTile.previousTiles[0]
+				this.updatePlayerPosition(player, player.currentTile.previousTiles[0]);
 			}
 		}
 	}
@@ -83,11 +92,11 @@ class Board {
 		for(let i = 0; i < game.rollDice(); i++) {
 			if (player.currentTile.isJunction) {	
 				player.sendMessage('chooseNextTile').then((response) => {
-					updatePlayerPosition(player, response);
-				}
+					this.updatePlayerPosition(player, response);
+				});
 			}
 			else {
-				updatePlayerPosition(player, player.currentTile.nextTiles[0]);
+				this.updatePlayerPosition(player, player.currentTile.nextTiles[0]);
 			}
 			
 			if (player.currentTile.isEndTile) {
@@ -97,15 +106,17 @@ class Board {
 	}
 	
 	jumpToStart(player) {
-		updatePlayerPosition(player, tiles[0]);
+		this.updatePlayerPosition(player, this.tiles[0]);
 	}
-	
+
+	// Insert previous direct neighbours for every tile in a path
 	setPrevious(path) {
 		for (let i = 1; i < path.length; i++) {
 			path[i].previousTiles.push(path[i - 1]);
 		}
 	}
-	
+
+	// Insert next direct neighbours for every tile in a path
 	setNext(path) {
 		for (let i = 0; i < path.length - 1; i++) {
 			path[i].nextTiles.push(path[i + 1]);
